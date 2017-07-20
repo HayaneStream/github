3c108c582723f30080f7ad4af0b97b87188c9dd7
Havanen <streamofempire@gmail.com>
2017-07-20 20:59:53 +0700

    update ex8

--- ex8/js/main.js
+++ ex8/js/main.js
@@ -1,16 +1,13 @@
-;(function () {
-	
-	'use strict';
-
-	var testFunction = function(){
-		console.log('script run');
-	};
-
-
-	// Document on load.
-	$(function(){
-		testFunction();
-	});
-
-
-}());
+function changetext1() {
+	 document.getElementById("from").innerHTML = "From:";
+   document.getElementById("check-in").innerHTML = "Check In:";
+   document.getElementById("class").innerHTML = "Class:";
+   document.getElementById("search").innerHTML = "Search Flight";
+}
+function changetext2() {
+	 document.getElementById("from").innerHTML = "City:";
+	 document.getElementById("from").removeAttr('to');
+   document.getElementById("check-in").innerHTML = "Return:";
+   document.getElementById("class").innerHTML = "Rooms:";
+   document.getElementById("search").innerHTML = "Search Hotel";
+}
