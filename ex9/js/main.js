0e68f99691a0491686f2eb64601b4f9da4e0e06c
Havanen <streamofempire@gmail.com>
2017-07-25 23:13:06 +0900

    ex9

---
+++ ex9/js/main.js
@@ -0,0 +1,15 @@
+function changeimage1() {
+   document.getElementById('image').src='https://s-media-cache-ak0.pinimg.com/originals/95/2f/fe/952ffe14d46b6997f6816d585693cb09.jpg';
+}
+function changeimage2() {
+   document.getElementById('image').src='http://www.canyon-news.com/wp-content/uploads/2016/03/Earth-Day.jpg';
+}
+function changeimage3() {
+   document.getElementById('image').src='http://4.bp.blogspot.com/-mnCM5rDmJq4/UCkhyoBDrjI/AAAAAAAAAGM/nBdUYHC10ws/s1600/scenery-mountain.jpeg';
+}
+function changeimage4() {
+   document.getElementById('image').src='https://1.bp.blogspot.com/-9OdV-4q9qQU/UjrjZg39S6I/AAAAAAAABXw/ctvxW2dTT20/s1600/Outdoor+Anime+Landscape+%5BScenery+-+Background%5D+69.png';
+}
+function changeimage5() {
+   document.getElementById('image').src='https://s-media-cache-ak0.pinimg.com/736x/1a/42/b7/1a42b70226c1841748f7a862aebd3626--scenery-wallpaper-desktop-wallpapers.jpg';
+}
